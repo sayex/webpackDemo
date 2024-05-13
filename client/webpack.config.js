08b5dd28e38c70607a20cf46a95e0fd5e126565c
Eric Sayer <me@ericsayer.com>
2024-05-13 11:55:41 -0600

    init

---
+++ client/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/js/index.js',
+  },
+  devServer: {
+    hot: 'only',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Client Server',
+      template: './src/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
--- server/server.js
+++ server/server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const routes = require("./routes");
 const app = express();
-const db = require("./config/connection")
+const db = require("./config/connection");
+const path = require('path');
 
 app.use(express.json());
+app.use(express.static('../client/dist'));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", routes)
 
+app.get('/', (req, res) =>
+	res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+);
 
 db.once("open", ()=>{
 	app.listen(PORT, ()=>{
